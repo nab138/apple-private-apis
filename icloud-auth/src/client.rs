d21975bf833b7a715a3276fe326fc95bbcf24a13
nab138 <nab@nabdev.me>
2025-09-20 13:41:12 -0400

    Remove unused log

--- icloud-auth/src/client.rs
+++ icloud-auth/src/client.rs
@@ -687,7 +687,7 @@ impl AppleAccount {
     }
 
     pub async fn verify_2fa(&self, code: String) -> Result<LoginState, Error> {
-        println!("Verifying 2fa Code {}", code.clone());
+        // println!("Verifying 2fa Code {}", code.clone());
         let headers = self.build_2fa_headers(false);
         // println!("Recieved code: {}", code);
         let res = self
@@ -700,7 +700,6 @@ impl AppleAccount {
             )
             .send()
             .await?;
-        println!("Response: {:?}", res);
 
         let res: plist::Dictionary = plist::from_bytes(res.text().await?.as_bytes())?;
 
